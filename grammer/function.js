34dccf5449d05520593cb63c31f07de5d20c7231
jieun0240 <w2303@e-mirim.hs.kr>
2025-03-14 11:52:07 +0900

    arrow function3

--- grammer/function.js
+++ grammer/function.js
@@ -4,13 +4,15 @@ function 함수(매개변수) {
 
 함수('호출')
 
-// arrow functio1
+// arrow function1
 익명함수 = (매개변수) => {
   console.log('익명함수' + 매개변수);
 }
-// arrow functio2 // 매개변수 하나일 때 사용
+// arrow function2 // 매개변수 하나일 때 사용
 익명함수 = 매개변수 => {
   console.log('익명함수' + 매개변수);
 }
+// arrow function3 // 한줄이거나 리턴
+익명함수 = 매개변수 => console.log('익명함수' + 매개변수);
 
 익명함수('swag')
