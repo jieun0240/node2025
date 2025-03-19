01fc19f7340a3b0d1922df4aac2baaaeaf908856
jieun0240 <w2303@e-mirim.hs.kr>
2025-03-19 10:09:06 +0900

    express 코드로 변환

--- app.js
+++ app.js
@@ -1,23 +1,3 @@
-// http 모듈을 불러옵니다.
-const http = require('http');
-
-// 서버 생성 (함수 안에 호출되는 함수 = 콜백 함수(내부 함수))
-const server = http.createServer((req, res) => {
-  // 응답 헤더 설정
-  // 200번대가 성공..? 그리고 'text/plain' 이거는 그냥 text로면 구성되어있음을 나타냄
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  // 클라이언트에게 메시지 응답
-  res.end('Hello, World!');
-});
-
-// 서버가 3000번 포트에서 요청을 받을 수 있도록 설정
-server.listen(3000, () => {
-  console.log('서버가 http://localhost:3000 에서 실행 중입니다.');
-});
-// locallhost(127.0.0.1) -> Domain Name Service -> 우리가 아는 그 링크로
-// DNS는 도메인 이름을 실제 인터넷에서 사용 가능한 IP 주소로 변환해주는 시스템임.
-
-
 // Express 모듈 불러오기
 const express = require('express');
 
@@ -29,7 +9,7 @@ app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
-// 서버 실행
+// 서버 실행 (3000번 포트에서 요청을 받음)
 app.listen(3000, () => {
   console.log('서버가 http://localhost:3000 에서 실행 중입니다.');
 });
--- grammer/function.js
+++ grammer/function.js
@@ -1,18 +1,30 @@
-function 함수(매개변수) {
-  console.log('함수' + 매개변수)
+// function 함수(매개변수) {
+//   console.log('함수' + 매개변수)
+// }
+
+// 함수('호출')
+
+// // arrow function1
+// 익명함수 = (매개변수) => {
+//   console.log('익명함수' + 매개변수);
+// }
+// // arrow function2 // 매개변수 하나일 때 사용
+// 익명함수 = 매개변수 => {
+//   console.log('익명함수' + 매개변수);
+// }
+// // arrow function3 // 한줄이거나 리턴
+// 익명함수 = 매개변수 => console.log('익명함수' + 매개변수);
+
+// 익명함수('swag')
+
+const fruits = ['apple', 'banana', 'orange'];
+
+for(let i = 0; i < fruits.length; i++) {
+  console.log(fruits[i]);
 }
 
-함수('호출')
+fruits.forEach(l =>console.log(l));
 
-// arrow function1
-익명함수 = (매개변수) => {
-  console.log('익명함수' + 매개변수);
+for(const l of fruits) {
+  console.log(l);
 }
-// arrow function2 // 매개변수 하나일 때 사용
-익명함수 = 매개변수 => {
-  console.log('익명함수' + 매개변수);
-}
-// arrow function3 // 한줄이거나 리턴
-익명함수 = 매개변수 => console.log('익명함수' + 매개변수);
-
-익명함수('swag')
