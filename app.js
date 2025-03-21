904e7dd9b9e8539c29da13333c1417407ce9ce67
jieun0240 <w2303@e-mirim.hs.kr>
2025-03-21 11:15:07 +0900

    post 요청

--- app.js
+++ app.js
@@ -3,13 +3,19 @@ const express = require('express');
 
 // Express 애플리케이션 생성
 const app = express();
+const port = 2007;
 
 // 기본 라우트 설정
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.get('/swag', (req, res) => {
+  res.send('get swag');
 });
 
-// 서버 실행 (3000번 포트에서 요청을 받음)
-app.listen(3000, () => {
-  console.log('서버가 http://localhost:3000 에서 실행 중입니다.');
+app.post('/swag', (req, res) => {
+  res.send('post swag');
+});
+
+
+// 서버 실행 (2007번 포트에서 요청을 받음)
+app.listen(port, () => {
+  console.log(`서버가 http://localhost:${port}/swag 에서 실행 중입니다.`);
 });
