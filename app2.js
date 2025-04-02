0fd96d732a4be35a7f270d12ff1efcc6c2d2f241
jieun0240 <w2303@e-mirim.hs.kr>
2025-04-02 09:32:30 +0900

    mysql 연결

--- app2.js
+++ app2.js
@@ -1,9 +1,25 @@
 const express = require('express');
-
+const mysql = require('mysql2');
 const path = require('path');
 
 const app = express();
 const port = 2027;
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'mirim4',
+    database: 'traveldb'
+});
+
+db.connect(err => {
+    if(err) {
+        console.error('MySQL 연결 실패 : ', err);
+        return;
+    }
+    console.log('MySQL 연결 성공');
+})
+
 app.set('view engine', 'ejs');
 
 // __dirname = 현재 파일이 속해있는 디렉토리의 절대경로
@@ -18,4 +34,4 @@ app.get('/travel', (req, res) => {
 
 app.listen(port, () => {
     console.log(`서버가 http://localhost:${port}/swag 에서 실행 중입니다.`);
-  });
+});
