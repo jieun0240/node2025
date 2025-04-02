df1c53a8753719227c7aa6250616b68b2191129e
jieun0240 <w2303@e-mirim.hs.kr>
2025-04-02 09:43:05 +0900

    .env

--- app2.js
+++ app2.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const mysql = require('mysql2');
 const path = require('path');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
 const port = 2027;
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'mirim4',
-    database: 'traveldb'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 db.connect(err => {
@@ -18,7 +20,7 @@ db.connect(err => {
         return;
     }
     console.log('MySQL 연결 성공');
-})
+});
 
 app.set('view engine', 'ejs');
 
