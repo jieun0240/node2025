6b8e9ff6b62e010893c056306acd7982f21a93f2
jieun0240 <w2303@e-mirim.hs.kr>
2025-04-04 11:44:08 +0900

    Read(Get) 완성

--- app2.js
+++ app2.js
@@ -1,46 +1,67 @@
 const express = require('express');
-const mysql = require('mysql2');
 const path = require('path');
+const mysql = require('mysql2');
 const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-const port = 2027;
+const port = 2007;
 
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
 db.connect(err => {
-    if(err) {
+    if (err) {
         console.error('MySQL 연결 실패 : ', err);
         return;
     }
-    console.log('MySQL 연결 성공');
-});
+    console.log('MySQL 연결 성공!!');
+})
 
 app.set('view engine', 'ejs');
 
-// __dirname = 현재 파일이 속해있는 디렉토리의 절대경로
-// path.join을 사용하여 운영체제의 맞추어 경로 지정자를 설정
+// __dirname : 현재 파일이 속해있는 디렉토리의 절대 경로
+//path.join : 운영체제에 맞추어 경로지정자(/ 혹은 \)를 설정해 준다.
 app.set('views', path.join(__dirname, 'views'));
+
+// 두 개의 결괏값이 같다.
+console.log(path.join(__dirname, 'views'));
+// console.log(__dirname + '\\views');
 
 app.get('/travel', (req, res) => {
     const _query = 'SELECT * FROM travellist';
     db.query(_query, (err, results) => {
-        if(err) {
-            console.error('데이터베이스 쿼리 실패', err);
+        if (err) {
+            console.error('데이터베이스 쿼리 실패 : ', err);
             res.status(500).send('Internal Server Error');
             return;
         }
         const travelList = results;
-        res.render('travel', {travelList});
+        res.render('travel', { travelList });
     });
 });
 
+
+app.get('/travel/:id', (req, res) => {
+    const travelId = req.params.id;
+    const _query = 'SELECT * FROM travellist WHERE id = ?';
+    db.query(_query, [travelId], (err, results) => {
+        if (err) {
+            console.error('데이터베이스 쿼리 실패 : ', err);
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+        const travel = results[0];
+        res.render('travelDetail', { travel });
+    });
+});
+
+
 app.listen(port, () => {
-    console.log(`서버가 http://localhost:${port}/swag 에서 실행 중입니다.`);
+    console.log(`Server running at http://localhost:${port}/`);
 });
