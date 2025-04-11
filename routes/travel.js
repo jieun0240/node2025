de12aa7b38c91b49802cdbfdbadc6d56b4ccdf7b
jieun0240 <w2303@e-mirim.hs.kr>
2025-04-11 11:09:33 +0900

    라우터 분리

---
+++ routes/travel.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const router = express.Router();
+
+module.exports = (db) => {
+    router.get('/', (req, res) => {
+        const _query = 'SELECT * FROM travellist';
+        db.query(_query, (err, results) => {
+            if (err) return res.status(500).send('Internal Server Error');
+            res.render('travel', { travelList: results });
+        });
+    });
+
+    router.get('/add', (req, res) => {
+        res.render('addTravel');
+    });
+
+    router.post('/', (req, res) => {
+        const { name } = req.body;
+        const _query = 'INSERT INTO travellist (name) VALUES (?)';
+        db.query(_query, [name], (err) => {
+            if (err) return res.status(500).send('Internal Server Error');
+            res.redirect('/travel');
+        });
+    });
+
+    router.get('/:id', (req, res) => {
+        const travelId = req.params.id;
+        const _query = 'SELECT * FROM travellist WHERE id = ?';
+        db.query(_query, [travelId], (err, results) => {
+            if (err) return res.status(500).send('Internal Server Error');
+            res.render('travelDetail', { travel: results[0] });
+        });
+    });
+
+    router.get('/:id/edit', (req, res) => {
+        const travelId = req.params.id;
+        const _query = 'SELECT * FROM travellist WHERE id = ?';
+        db.query(_query, [travelId], (err, results) => {
+            if (err) return res.status(500).send('Internal Server Error');
+            res.render('editTravel', { travel: results[0] });
+        });
+    });
+
+    router.put('/:id', (req, res) => {
+        const travelId = req.params.id;
+        const { name } = req.body;
+        const _query = 'UPDATE travellist SET name = ? WHERE id = ?';
+        db.query(_query, [name, travelId], (err) => {
+            if (err) return res.status(500).send('Internal Server Error');
+            res.render('updateSuccess');
+        });
+    });
+
+    router.delete('/:id', (req, res) => {
+        const travelId = req.params.id;
+        const _query = 'DELETE FROM travellist WHERE id = ?';
+        db.query(_query, [travelId], (err) => {
+            if (err) return res.status(500).send('Internal Server Error');
+            res.render('deleteSuccess');
+        });
+    });
+
+    return router;
+};
